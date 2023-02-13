0ff139139adad0fb8fa74494d4910c01e8b7672a
reeman9000 <preston.soper@student.awesomeinc.org>
2023-02-13 19:14:37 +0000

    messing with npm

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
-// main.js
+// const axios = require('axios');
+let url = 'https://official-joke-api.appspot.com/random_ten';
+let jokes = [];
 
-function init(){
-    console.log("hello world!");
+console.log(axios.get(url));
+axios.get(url)
+  .then(function (response) {
+    // handle success
+    // console.log(response);
+    // console.log('JOKES', response.data);
+    jokes = response.data;
+    console.log(jokes);
+    createListItems(jokes);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  });
+
+
+function createListItems(arr) {
+    for (const joke of arr) {
+        let listItem = document.createElement('li');
+        listItem.innerText = `${joke.setup}: ${joke.punchline}`;
+        document.getElementById('myList').appendChild(listItem);
+    }
 }
