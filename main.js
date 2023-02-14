a84dd8a3ac3fe01503885cc0ccccda898f100371
reeman9000 <preston.soper@student.awesomeinc.org>
2023-02-14 19:35:25 +0000

    nuke and pave

--- main.js
+++ main.js
@@ -1,3 +1,50 @@
+let url = 'https://official-joke-api.appspot.com/random_ten';
+let jokes = "";
+let getJoke = document.getElementById("getJoke");
+
+console.log(axios.get(url));
+
+function getJokes () {
+  axios.get(url)
+  .then(function (response) {
+    // handle success
+    // console.log(response);
+    // console.log('JOKES', response.data);
+    jokes = response.data;
+    console.log(jokes);
+    createListItems(jokes);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  });
+}
+// axios.get(url)
+//   .then(function (response) {
+//     // handle success
+//     // console.log(response);
+//     // console.log('JOKES', response.data);
+//     jokes = response.data;
+//     console.log(jokes);
+//     createListItems(jokes);
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log(error);
+//   });
+
+
+function createListItems(jokes) {
+    for (const joke of jokes) {
+        let listItem = document.createElement('li');
+        listItem.innerText = `${joke.setup}: ${joke.punchline}`;
+        document.getElementById('myList').appendChild(listItem);
+    }
+}
+
+
+
+getJoke.addEventListener("click", () => getJokes());
 
 
 
