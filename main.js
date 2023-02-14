f738c457f09c0c26d097ffacb1c2707fff692495
reeman9000 <preston.soper@student.awesomeinc.org>
2023-02-14 17:58:12 +0000

    commented out javascript

--- main.js
+++ main.js
@@ -1,27 +1,55 @@
-// const axios = require('axios');
-let url = 'https://official-joke-api.appspot.com/random_ten';
-let jokes = [];
-
-console.log(axios.get(url));
-axios.get(url)
-  .then(function (response) {
-    // handle success
-    // console.log(response);
-    // console.log('JOKES', response.data);
-    jokes = response.data;
-    console.log(jokes);
-    createListItems(jokes);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  });
 
 
-function createListItems(arr) {
-    for (const joke of arr) {
-        let listItem = document.createElement('li');
-        listItem.innerText = `${joke.setup}: ${joke.punchline}`;
-        document.getElementById('myList').appendChild(listItem);
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // const axios = require('axios');
+// let url = 'https://official-joke-api.appspot.com/random_ten';
+// let jokes = [];
+
+// console.log(axios.get(url));
+// axios.get(url)
+//   .then(function (response) {
+//     // handle success
+//     // console.log(response);
+//     // console.log('JOKES', response.data);
+//     jokes = response.data;
+//     console.log(jokes);
+//     createListItems(jokes);
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log(error);
+//   });
+
+
+// function createListItems(arr) {
+//     for (const joke of arr) {
+//         let listItem = document.createElement('li');
+//         listItem.innerText = `${joke.setup}: ${joke.punchline}`;
+//         document.getElementById('myList').appendChild(listItem);
+//     }
+// }
